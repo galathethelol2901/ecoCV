be6fab85547e4088ea74ff08cc5e944f99b0ecf5
galathe <ymettoudi@yahoo.fr>
2023-02-11 03:24:18 +0100

    new

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>BIENVENUE SUR MONOPROG6 LE JEUX !</title>
+  <link rel="stylesheet" href="indexMpp.css">
+</head>
+
+<body>
+  <div class="container">
+    <div class="section-1"></div>
+    <h1>hello world</h1>
+
+    <div class="section-2"></div>
+    <h2>hello mes couille</h2>
+
+    <div class="section-3"></div>
+    <h3>salut la miff</h3>
+  </div>
+</body>
+
+</html>
