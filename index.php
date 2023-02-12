59a078092cc058f38eb1a26c8732e2d9c564b80c
galathe <ymettoudi@yahoo.fr>
2023-02-12 04:00:15 +0100

    nws1

--- index.php
+++ index.php
@@ -10,22 +10,53 @@
 <body>
 
 <a href="https://www.mythrillfiction.com/the-dark-rider" alt="Mythrill" target="_blank">
+
   <div class="card">
+
     <div class="wrapper">
-      <img src="https://ggayane.github.io/css-experiments/cards/dark_rider-cover.jpg" class="cover-image" />
+
+      <img src="https://ggayane.github.io/css-experiments/cards/dark_rider-cover.jpg" 
+      class="cover-image" />
+
     </div>
+
     <img src="https://ggayane.github.io/css-experiments/cards/dark_rider-title.png" class="title" />
     <img src="https://ggayane.github.io/css-experiments/cards/dark_rider-character.webp" class="character" />
+
   </div>
 </a>
 
 <a href="https://www.mythrillfiction.com/force-mage" alt="Mythrill" target="_blank">
+
   <div class="card">
+
     <div class="wrapper">
-      <img src="https://ggayane.github.io/css-experiments/cards/force_mage-cover.jpg" class="cover-image" />
+
+      <img src="https://ggayane.github.io/css-experiments/cards/force_mage-cover.jpg" 
+      class="cover-image" />
+
     </div>
+
     <img src="https://ggayane.github.io/css-experiments/cards/force_mage-title.png" class="title" />
     <img src="https://ggayane.github.io/css-experiments/cards/force_mage-character.webp" class="character" />
+
+  </div>
+</a>
+
+<a href="https://www.mythrillfiction.com/the-dark-rider" alt="Mythrill" target="_blank">
+
+  <div class="card">
+
+    <div class="wrapper">
+
+      <img src="fireOrb.jpg" 
+      class="cover-image" />
+
+    </div>
+
+    <img src="champH.png" class="title" />
+    <img src="champHland.jpg" class="character" />
+
   </div>
 </a>
   
