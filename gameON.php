4ff0560cdadc04e353e0a37694870718901d022a
galathe <ymettoudi@yahoo.fr>
2023-02-21 08:28:20 +0100

    neeeew

--- gameON.php
+++ gameON.php
@@ -12,18 +12,23 @@
 
 <div class="game-grid">
   <div class="player player-1">
-    <div class="base"></div>
+    <div class="base">
+      <img src="https://example.com/player-1-image.png" alt="Player 1 image">
+    </div>
   </div>
   <div class="player player-2">
-    <div class="base"></div>
+    <div class="base">
+      <img src="https://example.com/player-2-image.png" alt="Player 2 image">
+    </div>
   </div>
   <div class="player player-3">
-    <div class="base"></div>
+    <div class="base">
+      <img src="https://example.com/player-3-image.png" alt="Player 3 image">
+    </div>
   </div>
-</div>
-
-<div class="game-center">
-  <div class="game-center__inner"></div>
+  <div class="game-center">
+    <div class="game-center__inner"></div>
+  </div>
 </div>
 
 
