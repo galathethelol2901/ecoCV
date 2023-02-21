d209ba1ecd16c28d948fdf5ba6df36f6fa699c2e
galathe <ymettoudi@yahoo.fr>
2023-02-21 09:45:23 +0100

    ludo

--- gameON.php
+++ gameON.php
@@ -7,13 +7,22 @@
 
 <body>
 
-  <div class="game-container">
-    <div class="center"></div>
-    <div class="path1"></div>
-    <div class="path2"></div>
-    <div class="path3"></div>
-    <div class="path4"></div>
-  </div>
+<div id="ludo">
+
+  <div id="box1" class="left-box box box-top"></div>
+  <div id="l-shape-up" class="l-shape"></div>
+
+  <div id="box2" class="right-box box box-top"></div>
+  <div id="l-shape-left" class="l-shape"></div>
+
+  <div id="middle-box" class="box"></div>
+  <div id="l-shape-right" class="l-shape"></div>
+
+  <div id="box3" class="left-box box box-bottom"></div>
+  <div id="l-shape-down" class="l-shape"></div>
+  
+  <div id="box4" class="right-box box box-bottom"></div>
+</div>
 
 </body>
 
