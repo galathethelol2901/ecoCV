60d2665224dd639c19f6f4930ef15ba2526544fa
galathe <ymettoudi@yahoo.fr>
2023-02-21 08:54:51 +0100

    neeewww

--- gameON.php
+++ gameON.php
@@ -1,44 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-  <meta charset="UTF-8">
-  <title>BIENVENUE SUR MONOPROG6 LE JEUX !</title>
-  <link rel="stylesheet" href="gameON1.css">
+  <title>Mon petit jeu</title>
+  <link rel="stylesheet" type="text/css" href="gameON1.css">
 </head>
 
-
 <body>
-
-<div class="game-grid">
-  <div class="player player-1">
-    <div class="base">
-      <img src="https://example.com/player-1-image.png" alt="Player 1 image">
-    </div>
+    
+  <div class="game-container">
+    <div class="center"></div>
+    <div class="path1"></div>
+    <div class="path2"></div>
+    <div class="path3"></div>
+    <div class="path4"></div>
   </div>
-  <div class="player player-2">
-    <div class="base">
-      <img src="https://example.com/player-2-image.png" alt="Player 2 image">
-    </div>
-  </div>
-  <div class="player player-3">
-    <div class="base">
-      <img src="https://example.com/player-3-image.png" alt="Player 3 image">
-    </div>
-  </div>
-  <div class="game-center">
-    <div class="game-center__inner"></div>
-  </div>
-</div>
-
-<div class="path">
-    <div class="path__segment path__segment-1"></div>
-    <div class="path__segment path__segment-2"></div>
-    <div class="path__segment path__segment-3"></div>
-    <div class="path__segment path__segment-4"></div>
-    <div class="path__segment path__segment-5"></div>
-  </div>
-</div>
-
 
 </body>
+
+</html>
