3b661eec037f79fbb5212fd5bcfa33ba070fc389
galathe <ymettoudi@yahoo.fr>
2023-03-04 19:51:45 +0100

    music

--- anime.js
+++ anime.js
@@ -320,3 +320,15 @@ window.addEventListener("resize", function () {
   mouse.y = canvas.height / 2;
 });
 
+const audio = document.getElementById("myAudio");
+const playButton = document.getElementById("playButton");
+
+playButton.addEventListener('click', () => {
+  if (audio.paused) {
+    audio.play();
+    playButton.textContent = 'Pause';
+  } else {
+    audio.pause();
+    playButton.textContent = 'Play';
+  }
+});
--- index.php
+++ index.php
@@ -7,31 +7,18 @@
   </head>
   <body>
 
-<script>
-
-function toggleSound() {
-  const audio = document.querySelector("#myAudio");
-  const muteButton = document.querySelector("#muteButton");
-  
-  if (audio.muted) {
-    audio.muted = false;
-    muteButton.textContent = "Mute";
-  } else {
-    audio.muted = true;
-    muteButton.textContent = "Unmute";
-  }
-} 
-</script>
-
-  <audio id="myAudio" src="musicGme.mp3" autoplay loop></audio>
-  <button onclick="toggleSound()" id="muteButton">Mute</button>
-
     <container>
       <canvas id="canvas1"></canvas>
       <article>
-        <span>Created by <a href="" target='blank'>yoan mettoudi</a>
+        <span>la music est genial ! enfin ça depend ... 
+
+        <audio id="myAudio">
+        <source src="musicGme.mp3" type="audio/mpeg">
+       </audio>
+       <button id="playButton">Play</button>
         </span>
       </article>
+      
     </container>
   </body>
   <footer>pour me contatcer suivez le lien ! <a href="" target='blank'>ici </a>
