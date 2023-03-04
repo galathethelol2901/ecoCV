c131a51269eae9639a3178e64b42e2498f5cf062
galathe <ymettoudi@yahoo.fr>
2023-03-04 18:49:55 +0100

    zolie

--- index.php
+++ index.php
@@ -1,35 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <title>BIENVENUE SUR MONOPROG6 LE JEUX !</title>
-  <link rel="stylesheet" href="gameON1.css">
-</head>
-
-<body>
-
-<container>
-
-  <canvas id="canvas1"></canvas>
-
-  <article>
-
-    <span>Created by <a href="" target='blank'>yoan mettoudi</a>
-  </span>
-
-  </article>
-
-</container>
-
-<script src="anime.js"></script>
-
-</body>
-
-<footer>pour me contatcer suivez le lien !  
-  <a href="" target='blank'>ici
-  </a>
-
-</footer>
-
+  <head>
+    <meta charset="UTF-8">
+    <title>BIENVENUE SUR MONOPROG6 LE JEUX !</title>
+    <link rel="stylesheet" href="gameON1.css">
+  </head>
+  <body>
+    <container>
+      <canvas id="canvas1"></canvas>
+      <article>
+        <span>Created by <a href="" target='blank'>yoan mettoudi</a>
+        </span>
+      </article>
+    </container>
+    <script src="anime.js"></script>
+  </body>
+  <footer>pour me contatcer suivez le lien ! <a href="" target='blank'>ici </a>
+  </footer>
 </html>
