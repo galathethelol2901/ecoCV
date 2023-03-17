fd17cf9baf50181fcb3ee4e5d85d03e4eb584b6a
galathe <ymettoudi@yahoo.fr>
2023-03-17 20:36:47 +0100

    anime

--- index.php
+++ index.php
@@ -2,23 +2,22 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Page en 3 colonnes</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Page avec effet mouse follow</title>
+    <link rel="stylesheet" type="text/css" href="gameON1.css">
   </head>
   <body>
-    <div class="wrapper">
-      <div class="column column1">
-        <h2>Colonne 1</h2>
-        <p>Contenu de la colonne 1</p>
-      </div>
-      <div class="column column2">
-        <h2>Colonne 2</h2>
-        <p>Contenu de la colonne 2</p>
-      </div>
-      <div class="column column3">
-        <h2>Colonne 3</h2>
-        <p>Contenu de la colonne 3</p>
-      </div>
+    <div id="vague" class="section">
+      <h1>Section vague</h1>
+      <div class="circle"></div>
     </div>
+    <div id="bulle" class="section">
+      <h1>Section bulle</h1>
+      <div class="circle"></div>
+    </div>
+    <div id="galaxie" class="section">
+      <h1>Section galaxie</h1>
+      <div class="circle"></div>
+    </div>
+    <script src="anime.js"></script>
   </body>
 </html>
