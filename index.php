df2dd5523d4665fa03a306c56ffb9714662ad5cf
galathe <ymettoudi@yahoo.fr>
2023-03-17 20:22:36 +0100

    colon

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
 <!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Page en colonnes</title>
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
-  <div class="container">
-    <div class="section section1"></div>
-    <div class="section section2"></div>
-    <div class="section section3"></div>
-  </div>
-</body>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Page en 3 colonnes</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <div class="wrapper">
+      <div class="column column1">
+        <h2>Colonne 1</h2>
+        <p>Contenu de la colonne 1</p>
+      </div>
+      <div class="column column2">
+        <h2>Colonne 2</h2>
+        <p>Contenu de la colonne 2</p>
+      </div>
+      <div class="column column3">
+        <h2>Colonne 3</h2>
+        <p>Contenu de la colonne 3</p>
+      </div>
+    </div>
+  </body>
 </html>
