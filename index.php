7f16333b5ff4a43c87b5a73f464e5d8acc69d101
galathe <ymettoudi@yahoo.fr>
2023-03-18 06:56:22 +0100

    culumn1

--- index.php
+++ index.php
@@ -4,11 +4,13 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Ma page d'accueil</title>
-  <link rel="stylesheet" href="gameON1.css">
+  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-  <section class="light-blue"></section>
-  <section class="blue"></section>
-  <section class="dark-blue"></section>
+  <div class="wrapper">
+    <section class="light-blue"></section>
+    <section class="blue"></section>
+    <section class="dark-blue"></section>
+  </div>
 </body>
 </html>
