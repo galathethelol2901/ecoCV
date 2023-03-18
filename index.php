8d52b6313f862fbbe5167559bc96ed97b5149353
galathe <ymettoudi@yahoo.fr>
2023-03-18 06:48:13 +0100

    culun

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Page avec effet mouse follow</title>
-    <link rel="stylesheet" type="text/css" href="gameON1.css">
-  </head>
-  <body>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ma page d'accueil</title>
+  <link rel="stylesheet" href="gameON1.css">
+</head>
+<body>
   <section class="light-blue"></section>
   <section class="blue"></section>
   <section class="dark-blue"></section>
