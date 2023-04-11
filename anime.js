3332d9eac94077037a1c5a44131e62155c42cf3a
galathe <ymettoudi@yahoo.fr>
2023-04-12 01:55:33 +0200

    anime

--- anime.js
+++ anime.js
@@ -1,7 +1,13 @@
-var animation = document.querySelector('.animation');
-animation.style.transform = 'translate(-100%, -100%)';
-
-setTimeout(function() {
-  animation.style.transition = 'all 1s linear';
-  animation.style.transform = 'translate(100%, 100%)';
-}, 1000);
+function animateLine() {
+    var animation = document.querySelector('.animation');
+    animation.style.transition = 'none';
+    animation.style.transform = 'translate(-100%, -100%) rotate(45deg)';
+  
+    setTimeout(function() {
+      animation.style.transition = 'all 1s linear';
+      animation.style.transform = 'translate(100%, 100%) rotate(45deg)';
+    }, 100);
+  }
+  
+  animateLine(); // exécute une fois l'animation pour la première fois
+  setInterval(animateLine, 2000); // répète l'animation toutes les 2 secondes
