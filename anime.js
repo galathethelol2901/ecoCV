43748a5d34422715d83ebb91fde1acbae748c3fe
galathe <ymettoudi@yahoo.fr>
2023-04-12 01:48:09 +0200

    loop

--- anime.js
+++ anime.js
@@ -1,7 +1,13 @@
-var animation = document.querySelector('.animation');
-animation.style.transform = 'translateX(-100%)';
-
-setTimeout(function() {
-  animation.style.transition = 'all 1s linear';
-  animation.style.transform = 'translateX(100%)';
-}, 1000);
+function animateLine() {
+    var animation = document.querySelector('.animation');
+    animation.style.transition = 'none';
+    animation.style.transform = 'translateX(-100%)';
+  
+    setTimeout(function() {
+      animation.style.transition = 'all 1s linear';
+      animation.style.transform = 'translateX(100%)';
+    }, 100);
+  }
+  
+  animateLine(); // exécute une fois l'animation pour la première fois
+  setInterval(animateLine, 2000); // répète l'animation toutes les 2 secondes
