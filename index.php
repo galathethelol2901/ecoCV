54cf388121cfe51133f1c299a9883f05f4dad282
galathe <ymettoudi@yahoo.fr>
2023-04-12 00:46:41 +0200

    eco 4

--- index.php
+++ index.php
@@ -20,29 +20,29 @@
     <a class="card" href="#">
       <div class="card__background" style="background-image: url(https://images.unsplash.com/photo-1557177324-56c542165309?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80)"></div>
       <div class="card__content">
-        <p class="card__category">LE CV</p>
-        <h3 class="card__heading">pour faire connaissance</h3>
+        <p class="card__category">pour me connaitre</p>
+        <h3 class="card__heading">le CV</h3>
       </div>
     </a>
     <a class="card" href="#">
       <div class="card__background" style="background-image: url(https://images.unsplash.com/photo-1557187666-4fd70cf76254?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)"></div>
       <div class="card__content">
-        <p class="card__category">LE FORMULAIRE </p>
-        <h3 class="card__heading">pour ce </h3>
+        <p class="card__category">pour vous decouvrire</p>
+        <h3 class="card__heading">Le FORMULAIRE</h3>
       </div>
     </a>
     <a class="card" href="#">
       <div class="card__background" style="background-image: url(https://images.unsplash.com/photo-1556680262-9990363a3e6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)"></div>
       <div class="card__content">
-        <p class="card__category">LE JEUX</p>
-        <h3 class="card__heading">pour ce detendre</h3>
+        <p class="card__category">pour ce detendre</p>
+        <h3 class="card__heading">Le JEUX</h3>
       </div>
     </li>
     <a class="card" href="#">
       <div class="card__background" style="background-image: url(https://images.unsplash.com/photo-1557004396-66e4174d7bf6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)"></div>
       <div class="card__content">
-        <p class="card__category">LE FILM </p>
-        <h3 class="card__heading">en traveaux </h3>
+        <p class="card__category">en travaux </p>
+        <h3 class="card__heading">Le FILM </h3>
       </div>
     </a>
   <div>
