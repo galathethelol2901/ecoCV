3659d7549a5524f7b89708821a76bf1155cdb6d8
galathe <ymettoudi@yahoo.fr>
2023-04-15 01:42:14 +0200

    eco1

--- HomeCV.php
+++ HomeCV.php
@@ -3,12 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <title>BIENVENUE SUR ECOPROG6</title>
-    <script
+
+        <script
             src="https://code.jquery.com/jquery-3.6.0.min.js"
             integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-            crossorigin="anonymous"></script>
-    <link href="bootstrap-4.4.1-dist/css/bootstrap.min.css" rel="stylesheet">
+            crossorigin="anonymous">
+        </script>
+
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" 
+            rel="stylesheet" 
+            integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" 
+            crossorigin="anonymous">
     <link rel="stylesheet" href="indexColor.css">
+    
     <script src="https://kit.fontawesome.com/3e7bbf4130.js" crossorigin="anonymous"></script>
 </head>
 
