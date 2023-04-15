75bef8195ce2448cb328d4b570795ad7c89a45cc
galathe <ymettoudi@yahoo.fr>
2023-04-15 04:19:50 +0200

    cdn

--- HomeCV.php
+++ HomeCV.php
@@ -6,16 +6,18 @@
     <script src="https://kit.fontawesome.com/3e7bbf4130.js" crossorigin="anonymous"></script>
     
 
-    <script src="https://code.jquery.com/jquery-3.6.4.js"
-     integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" 
-     crossorigin="anonymous">
-    </script>
+            <script src="https://code.jquery.com/jquery-3.6.4.js"
+             integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" 
+             crossorigin="anonymous">
+           </script>
 
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" 
             rel="stylesheet" 
             integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" 
             crossorigin="anonymous">
-    <link rel="stylesheet" href="indexColor.css">
+
+           <link rel="stylesheet" href="indexColor.css">
+
 </head>
 
 <body>
