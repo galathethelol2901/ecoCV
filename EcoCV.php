9e19058fe60fe1307ff48f756146de7e0532667e
galathe <ymettoudi@yahoo.fr>
2023-04-17 11:11:02 +0200

    cece

--- EcoCV.php
+++ EcoCV.php
@@ -205,6 +205,12 @@
         </div>
     </div>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" 
+crossorigin="anonymous">
+</script>
+
+<script src="jquery-3.6.4.min.js"></script> 
 
 
 <script type="text/javascript">
@@ -226,13 +232,6 @@
     });
 
 </script>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" 
-integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" 
-crossorigin="anonymous">
-</script>
-
-<script src="jquery-3.6.4.min.js"></script> 
 
 </body>
 
