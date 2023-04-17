026cc8817ecf7b0c3f7fe72e2538baf5e5d74374
galathe <ymettoudi@yahoo.fr>
2023-04-17 17:06:36 +0200

    bootstrap

--- EcoCV.php
+++ EcoCV.php
@@ -39,6 +39,6 @@
   src="https://code.jquery.com/jquery-3.6.4.js"
   integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E="
   crossorigin="anonymous"></script>
-
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-IFh35VGgLtYjTtTAdE0r1NTtCr2j+95XlHynhZxFkPZDxPqYwfaa/Dp1aJhGMVoG" crossorigin="anonymous"></script>
 </body>
 </html>
