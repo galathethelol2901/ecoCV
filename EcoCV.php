b898517c0d66bf5444a9eb1c3d40b4bd98810625
galathe <ymettoudi@yahoo.fr>
2023-04-17 17:15:26 +0200

    modal

--- EcoCV.php
+++ EcoCV.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Test Modal Bootstrap</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-MQ2IcEzVOWBAjhDJb9vjvjt8kOLWZ0tFQNNaAmgLgKx8AjiwqGtHq/3q9l9g8fBU" crossorigin="anonymous"></script>
   <!-- Inclure les fichiers CSS de Bootstrap -->
 </head>
 <body>
