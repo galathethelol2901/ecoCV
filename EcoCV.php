bbd07ca5a6b2c91adc35d95b206cf8e832021bd1
galathe <ymettoudi@yahoo.fr>
2023-04-17 17:11:33 +0200

    modal

--- EcoCV.php
+++ EcoCV.php
@@ -5,7 +5,6 @@
   <title>Test Modal Bootstrap</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <!-- Inclure les fichiers CSS de Bootstrap -->
-
 </head>
 <body>
 
@@ -34,11 +33,10 @@
       </div>
     </div>
   </div>
+
   <!-- Inclure jQuery et Bootstrap JS -->
-  <script
-  src="https://code.jquery.com/jquery-3.6.4.js"
-  integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E="
-  crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-IFh35VGgLtYjTtTAdE0r1NTtCr2j+95XlHynhZxFkPZDxPqYwfaa/Dp1aJhGMVoG" crossorigin="anonymous"></script>
+
 </body>
 </html>
