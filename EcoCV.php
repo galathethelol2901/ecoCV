a55ea2dfa237bfce012dc8aa5e4468c5871a0cb0
galathe <ymettoudi@yahoo.fr>
2023-04-17 11:26:44 +0200

    npppm

--- EcoCV.php
+++ EcoCV.php
@@ -6,24 +6,6 @@
 
     <link rel="stylesheet" href="EcoCVstyle.css">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" 
-    rel="stylesheet" 
-    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" 
-    crossorigin="anonymous">
-
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js" 
-    integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" 
-    crossorigin="anonymous"></script>
-
-    <script src="https://kit.fontawesome.com/3e7bbf4130.js" crossorigin="anonymous"></script>
-
-    <script src="
-https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js
-"></script>
-<link href="
-https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css
-" rel="stylesheet">
-
 </head>
 
 <body>
@@ -210,14 +192,6 @@ https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css
             </div>
         </div>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" 
-integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" 
-crossorigin="anonymous">
-</script>
-
-<script src="jquery-3.6.4.min.js"></script> 
-
 
 <script type="text/javascript">
 
