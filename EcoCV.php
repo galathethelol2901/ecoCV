b2c96a8ccd75e490dad691e70d9c3300d029fe68
galathe <ymettoudi@yahoo.fr>
2023-04-17 17:18:48 +0200

    ddn

--- EcoCV.php
+++ EcoCV.php
@@ -3,17 +3,13 @@
 <head>
   <meta charset="UTF-8">
   <title>Test Modal Bootstrap</title>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-MQ2IcEzVOWBAjhDJb9vjvjt8kOLWZ0tFQNNaAmgLgKx8AjiwqGtHq/3q9l9g8fBU" crossorigin="anonymous"></script>
-  <!-- Inclure les fichiers CSS de Bootstrap -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
 </head>
 <body>
-
-  <!-- Bouton de déclenchement du modal -->
   <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
     Ouvrir le modal
   </button>
-
-  <!-- Modal -->
   <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
@@ -33,10 +29,9 @@
       </div>
     </div>
   </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
-  <!-- Inclure jQuery et Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-IFh35VGgLtYjTtTAdE0r1NTtCr2j+95XlHynhZxFkPZDxPqYwfaa/Dp1aJhGMVoG" crossorigin="anonymous"></script>
-
+ 
 </body>
 </html>
