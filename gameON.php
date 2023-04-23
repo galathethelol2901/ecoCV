ed698ab733c3ce3b17824ff78abfbe30054e3602
galathe2901 <csmaf.gouv@yahoo.fr>
2023-04-23 07:38:08 +0200

    ndnd

--- gameON.php
+++ gameON.php
@@ -5,18 +5,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>le petit jeu</title>
     <link rel="stylesheet" href="GameStyle.css">
-    <script src="scriptGame.js"></script>
 </head>
 <body>
 <container>
   <canvas id="canvas1"></canvas>
-
-  <article><span>Created by <a href="https://www.youtube.com/channel/UCEqc149iR-ALYkGM6TG-7vQ" target='blank'>Frank's Laboratory</a> @2020</span>
-  </article>
 </container>
-
-
 <footer>Full step by step tutorial available <a href="https://www.youtube.com/channel/UCEqc149iR-ALYkGM6TG-7vQ" target='blank'>here</a>!
 </footer>
-
+<script src="scriptGame.js"></script>
 </body>
