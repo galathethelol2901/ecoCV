31cf351cfaf49f142839825ab9834520c50beadc
galathe2901 <csmaf.gouv@yahoo.fr>
2023-04-23 07:33:32 +0200

    cncn

--- gameON.php
+++ gameON.php
@@ -3,10 +3,9 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Ma page d'accueil</title>
-    <link rel="stylesheet" href="gameON.php">
+  <title>le petit jeu</title>
+    <link rel="stylesheet" href="gameStyle.css">
     <script src="scriptGame.js"></script>
-    <script src="https://kit.fontawesome.com/3e7bbf4130.js" crossorigin="anonymous"></script>
 </head>
 <body>
 <container>
