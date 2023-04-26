721d55e6cbe4e3fe4d1305dae9a81678cf0e4238
galathe2901 <csmaf.gouv@yahoo.fr>
2023-04-26 05:42:33 +0200

    cenrn

--- Quizz.js
+++ Quizz.js
@@ -1,37 +1,23 @@
-// On récupère toutes les questions et le bouton "Suivant"
-const questions = document.querySelectorAll('.question');
-const nextButton = document.querySelector('.next-button');
-
-// On cache toutes les questions sauf la première
-for (let i = 1; i < questions.length; i++) {
-  questions[i].classList.remove('active');
+var question_actuelle = 1;
+function suivant() {
+  if (question_actuelle == 1) {
+    document.getElementById("question1").style.display = "none";
+    document.getElementById("question2").style.display = "block";
+    question_actuelle = 2;
+  } else if (question_actuelle == 2) {
+    document.getElementById("question2").style.display = "none";
+    document.getElementById("question3").style.display = "block";
+    question_actuelle = 3;
+  }
 }
-
-// On ajoute un écouteur d'événement sur le bouton "Suivant"
-nextButton.addEventListener('click', showNextQuestion);
-
-// Fonction pour afficher la prochaine question
-function showNextQuestion() {
-  // On trouve la question actuellement active
-  const currentQuestion = document.querySelector('.question.active');
-
-  // On trouve la réponse sélectionnée par l'utilisateur
-  const selectedAnswer = currentQuestion.querySelector('input:checked');
-
-  // Si l'utilisateur n'a pas sélectionné de réponse, on ne fait rien
-  if (!selectedAnswer) {
-    return;
-  }
-
-  // On affiche la réponse sélectionnée par l'utilisateur
-  const selectedAnswerText = selectedAnswer.labels[0].textContent;
-  const selectedAnswerContainer = currentQuestion.querySelector('.selected-answer');
-  selectedAnswerContainer.textContent = 'Vous avez choisi : ' + selectedAnswerText;
-
-  // On cache la question actuellement active et on affiche la prochaine question
-  currentQuestion.classList.remove('active');
-  const nextQuestion = currentQuestion.nextElementSibling;
-  if (nextQuestion) {
-    nextQuestion.classList.add('active');
-  }
+function soumettre() {
+  // récupérer les réponses des questions
+  var reponse1 = document.querySelector('input[name="q1"]:checked').value;
+  var reponse2 = document.querySelector('input[name="q2"]:checked').value;
+  var reponse3 = document.querySelector('input[name="q3"]:checked').value;
+  
+  // faire quelque chose avec les réponses (par exemple les envoyer à un serveur)
+  console.log("Réponse 1 : " + reponse1);
+  console.log("Réponse 2 : " + reponse2);
+  console.log("Réponse 3 : " + reponse3);
 }
--- Quizz.php
+++ Quizz.php
@@ -8,58 +8,33 @@
     <script defer src="Quizz.js"></script>
 </head>
 <body>
-    <header>
-
-    </header> 
-
-    <div class="quiz-container">
-  <div class="question active">
-    <h2>Question 1: Quelle est votre couleur préférée ?</h2>
-    <input type="radio" id="option1" name="color" value="rouge">
-    <label for="option1">Rouge</label><br>
-    <input type="radio" id="option2" name="color" value="bleu">
-    <label for="option2">Bleu</label><br>
-    <input type="radio" id="option3" name="color" value="vert">
-    <label for="option3">Vert</label><br>
-    <p class="selected-answer"></p>
-    <button class="next-button" onclick="showNextQuestion()">Suivant</button>
+    
+<div id="questionnaire">
+  <h2>Questionnaire</h2>
+  <div id="question1">
+    <p>Question 1 : Quelle est votre couleur préférée ?</p>
+    <label><input type="radio" name="q1" value="rouge">Rouge</label><br>
+    <label><input type="radio" name="q1" value="bleu">Bleu</label><br>
+    <label><input type="radio" name="q1" value="vert">Vert</label><br>
+    <button onclick="suivant()">Suivant</button>
   </div>
-
-  <div class="question">
-    <h2>Question 2: Quel est votre animal préféré ?</h2>
-    <input type="radio" id="option4" name="animal" value="chien">
-    <label for="option4">Chien</label><br>
-    <input type="radio" id="option5" name="animal" value="chat">
-    <label for="option5">Chat</label><br>
-    <input type="radio" id="option6" name="animal" value="oiseau">
-    <label for="option6">Oiseau</label><br>
-    <p class="selected-answer"></p>
-    <button class="next-button" onclick="showNextQuestion()">Suivant</button>
+  <div id="question2" style="display:none;">
+    <p>Question 2 : Quel est votre animal préféré ?</p>
+    <label><input type="radio" name="q2" value="chien">Chien</label><br>
+    <label><input type="radio" name="q2" value="chat">Chat</label><br>
+    <label><input type="radio" name="q2" value="oiseau">Oiseau</label><br>
+    <button onclick="suivant()">Suivant</button>
   </div>
-
-  <div class="question">
-    <h2>Question 3: Quelle est votre nourriture préférée ?</h2>
-    <input type="radio" id="option7" name="food" value="pizza">
-    <label for="option7">Pizza</label><br>
-    <input type="radio" id="option8" name="food" value="hamburger">
-    <label for="option8">Hamburger</label><br>
-    <input type="radio" id="option9" name="food" value="sushi">
-    <label for="option9">Sushi</label><br>
-    <p class="selected-answer"></p>
-    <button class="next-button" onclick="showNextQuestion()">Suivant</button>
+  <div id="question3" style="display:none;">
+    <p>Question 3 : Quel est votre plat préféré ?</p>
+    <label><input type="radio" name="q3" value="pizza">Pizza</label><br>
+    <label><input type="radio" name="q3" value="pâtes">Pâtes</label><br>
+    <label><input type="radio" name="q3" value="sushi">Sushi</label><br>
+    <button onclick="soumettre()">Soumettre</button>
   </div>
+</div>
 
-  <div class="question">
-    <h2>Question 4: Quelle est votre saison préférée ?</h2>
-    <input type="radio" id="option10" name="season" value="été">
-    <label for="option10">Été</label><br>
-    <input type="radio" id="option11" name="season" value="automne">
-    <label for="option11">Automne</label><br>
-    <input type="radio" id="option12" name="season" value="hiver">
-    <label for="option12">Hiver</label><br>
-    <p class="selected-answer"></p>
-    <button class="next-button" onclick="showNextQuestion()">Suivant</button>
-  </div>
+  
 
 </body>
 </html>
