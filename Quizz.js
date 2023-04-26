44cae75441dec8a0aee14699f8ea8fa005ce0421
galathe2901 <csmaf.gouv@yahoo.fr>
2023-04-26 05:34:45 +0200

    ncndc

--- Quizz.js
+++ Quizz.js
@@ -1,29 +1,37 @@
-let questions = document.querySelectorAll('.question');
-let currentQuestion = 0;
+// On récupère toutes les questions et le bouton "Suivant"
+const questions = document.querySelectorAll('.question');
+const nextButton = document.querySelector('.next-button');
 
-function showNextQuestion() {
-  questions[currentQuestion].classList.remove('active');
-  currentQuestion++;
-  if (currentQuestion < questions.length) {
-    questions[currentQuestion].classList.add('active');
-  } else {
-    // Toutes les questions ont été posées
-    displayResult();
-  }
+// On cache toutes les questions sauf la première
+for (let i = 1; i < questions.length; i++) {
+  questions[i].classList.remove('active');
 }
 
-function displayResult() {
-  let selectedAnswers = document.querySelectorAll('input:checked');
-  let result = document.getElementById('result');
-  if (selectedAnswers.length === 5) {
-    // Toutes les questions ont une réponse sélectionnée
-    let animal = selectedAnswers[0].value;
-    let food = selectedAnswers[1].value;
-    let season = selectedAnswers[2].value;
-    let color = selectedAnswers[3].value;
-    let movie = selectedAnswers[4].value;
-    result.innerHTML = `Vous avez choisi : ${animal}, ${food}, ${season}, ${color}, ${movie}.`;
-  } else {
-    result.innerHTML = 'Veuillez répondre à toutes les questions.';
+// On ajoute un écouteur d'événement sur le bouton "Suivant"
+nextButton.addEventListener('click', showNextQuestion);
+
+// Fonction pour afficher la prochaine question
+function showNextQuestion() {
+  // On trouve la question actuellement active
+  const currentQuestion = document.querySelector('.question.active');
+
+  // On trouve la réponse sélectionnée par l'utilisateur
+  const selectedAnswer = currentQuestion.querySelector('input:checked');
+
+  // Si l'utilisateur n'a pas sélectionné de réponse, on ne fait rien
+  if (!selectedAnswer) {
+    return;
+  }
+
+  // On affiche la réponse sélectionnée par l'utilisateur
+  const selectedAnswerText = selectedAnswer.labels[0].textContent;
+  const selectedAnswerContainer = currentQuestion.querySelector('.selected-answer');
+  selectedAnswerContainer.textContent = 'Vous avez choisi : ' + selectedAnswerText;
+
+  // On cache la question actuellement active et on affiche la prochaine question
+  currentQuestion.classList.remove('active');
+  const nextQuestion = currentQuestion.nextElementSibling;
+  if (nextQuestion) {
+    nextQuestion.classList.add('active');
   }
 }
--- Quizz.php
+++ Quizz.php
@@ -14,49 +14,58 @@
     <div id="container-text" class="container-text">
     <h2>Ecoprog6 le QUIZZ ! </h2>
     <p>5 question pas de mauvaise reponse !</p>
+
+    <div class="quiz-container">
+  <div class="question active">
+    <h2>Question 1: Quelle est votre couleur préférée ?</h2>
+    <input type="radio" id="option1" name="color" value="rouge">
+    <label for="option1">Rouge</label><br>
+    <input type="radio" id="option2" name="color" value="bleu">
+    <label for="option2">Bleu</label><br>
+    <input type="radio" id="option3" name="color" value="vert">
+    <label for="option3">Vert</label><br>
+    <p class="selected-answer"></p>
+    <button class="next-button" onclick="showNextQuestion()">Suivant</button>
+  </div>
+
+  <div class="question">
+    <h2>Question 2: Quel est votre animal préféré ?</h2>
+    <input type="radio" id="option4" name="animal" value="chien">
+    <label for="option4">Chien</label><br>
+    <input type="radio" id="option5" name="animal" value="chat">
+    <label for="option5">Chat</label><br>
+    <input type="radio" id="option6" name="animal" value="oiseau">
+    <label for="option6">Oiseau</label><br>
+    <p class="selected-answer"></p>
+    <button class="next-button" onclick="showNextQuestion()">Suivant</button>
+  </div>
+
+  <div class="question">
+    <h2>Question 3: Quelle est votre nourriture préférée ?</h2>
+    <input type="radio" id="option7" name="food" value="pizza">
+    <label for="option7">Pizza</label><br>
+    <input type="radio" id="option8" name="food" value="hamburger">
+    <label for="option8">Hamburger</label><br>
+    <input type="radio" id="option9" name="food" value="sushi">
+    <label for="option9">Sushi</label><br>
+    <p class="selected-answer"></p>
+    <button class="next-button" onclick="showNextQuestion()">Suivant</button>
+  </div>
+
+  <div class="question">
+    <h2>Question 4: Quelle est votre saison préférée ?</h2>
+    <input type="radio" id="option10" name="season" value="été">
+    <label for="option10">Été</label><br>
+    <input type="radio" id="option11" name="season" value="automne">
+    <label for="option11">Automne</label><br>
+    <input type="radio" id="option12" name="season" value="hiver">
+    <label for="option12">Hiver</label><br>
+    <p class="selected-answer"></p>
+    <button class="next-button" onclick="showNextQuestion()">Suivant</button>
+  </div>
+  
 </div>
 
-<div id="quiz-container">
-  <div class="question active">
-    <h2>Question 1: Quel est votre animal préféré ?</h2>
-    <input type="radio" id="option1" name="animal" value="chien">
-    <label for="option1">Chien</label><br>
-    <input type="radio" id="option2" name="animal" value="chat">
-    <label for="option2">Chat</label><br>
-    <input type="radio" id="option3" name="animal" value="oiseau">
-    <label for="option3">Oiseau</label><br>
-    <button class="next-button" onclick="showNextQuestion()">Suivant</button>
-  </div>
-  <div class="question">
-    <h2>Question 2: Quel est votre plat préféré ?</h2>
-    <input type="radio" id="option4" name="food" value="pizza">
-    <label for="option4">Pizza</label><br>
-    <input type="radio" id="option5" name="food" value="spaghetti">
-    <label for="option5">Spaghetti</label><br>
-    <input type="radio" id="option6" name="food" value="burger">
-    <label for="option6">Burger</label><br>
-    <button class="next-button" onclick="showNextQuestion()">Suivant</button>
-  </div>
-  <div class="question">
-    <h2>Question 3: Quel est votre saison préférée ?</h2>
-    <input type="radio" id="option7" name="season" value="printemps">
-    <label for="option7">Printemps</label><br>
-    <input type="radio" id="option8" name="season" value="été">
-    <label for="option8">Été</label><br>
-    <input type="radio" id="option9" name="season" value="automne">
-    <label for="option9">Automne</label><br>
-    <button class="next-button" onclick="showNextQuestion()">Suivant</button>
-  </div>
-  <div class="question">
-    <h2>Question 4: Quelle est votre couleur préférée ?</h2>
-    <input type="radio" id="option10" name="color" value="rouge">
-    <label for="option10">Rouge</label><br>
-    <input type="radio" id="option11" name="color" value="bleu">
-    <label for="option11">Bleu</label><br>
-    <input type="radio" id="option12" name="color" value="vert">
-    <label for="option12">Vert</label><br>
-    <button class="next-button" onclick="showNextQuestion()">Suivant</button>
-  </div>
 
 </body>
 </html>
