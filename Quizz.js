4b4cc7f65e0c4d1361350c1562822da3ebfa1647
galathe2901 <csmaf.gouv@yahoo.fr>
2023-04-26 06:20:19 +0200

    quizz

--- Quizz.js
+++ Quizz.js
@@ -1,31 +1,90 @@
-// Récupérer le formulaire de quiz et le div pour afficher le score
-const quizForm = document.getElementById("quiz-form");
-const scoreDisplay = document.getElementById("score");
-
-// Initialiser les variables pour stocker les réponses de l'utilisateur et le score total
-let userResponses = [];
-let totalScore = 0;
-
-// Ajouter un écouteur d'événements pour soumettre le formulaire
-quizForm.addEventListener("submit", function(event) {
-    event.preventDefault(); // Empêcher la soumission du formulaire par défaut
-
-    // Récupérer les réponses de l'utilisateur et les stocker dans un tableau
-    userResponses.push(quizForm.elements.q1.value);
-    userResponses.push(quizForm.elements.q2.value);
-    userResponses.push(quizForm.elements.q3.value);
-
-    // Calculer le score total en fonction des réponses de l'utilisateur
-    for (let i = 0; i < userResponses.length; i++) {
-        if (userResponses[i] === "Vert") {
-            totalScore += 1;
-        } else if (userResponses[i] === "Chien") {
-            totalScore += 2;
-        } else if (userResponses[i] === "Sushi") {
-            totalScore += 3;
-        }
+// Définir les questions et les réponses
+const questions = [
+    {
+      question: "Quelle est votre couleur préférée ?",
+      answers: ["Bleu", "Jaune", "Rouge", "Vert"],
+      correctAnswer: "Vert",
+      points: 5
+    },
+    {
+      question: "Quel est votre animal préféré ?",
+      answers: ["Chat", "Chien", "Oiseau", "Lion"],
+      correctAnswer: "Chien",
+      points: 5
+    },
+    {
+      question: "Quel est votre plat préféré ?",
+      answers: ["Pizza", "Sushi", "Burger", "Poulet rôti"],
+      correctAnswer: "Sushi",
+      points: 5
     }
-
-    // Afficher le score final à l'utilisateur
-    scoreDisplay.textContent = "Votre score est de " + totalScore + " points.";
-});
+  ];
+  
+  // Définir les variables
+  let currentQuestion = 0;
+  let userScore = 0;
+  
+  // Fonction pour afficher la question et les réponses
+  function displayQuestion() {
+    // Récupérer la question et les réponses
+    const question = questions[currentQuestion].question;
+    const answers = questions[currentQuestion].answers;
+  
+    // Afficher la question et les réponses
+    $("#question").text(question);
+    $("#answers").empty();
+    for (let i = 0; i < answers.length; i++) {
+      $("#answers").append("<li><button class='answer-button'>" + answers[i] + "</button></li>");
+    }
+  
+    // Cacher le bouton Suivant et le résultat
+    $("#next-button").hide();
+    $("#result").hide();
+  
+    // Ajouter un écouteur d'événements pour les boutons de réponse
+    $(".answer-button").on("click", function() {
+      // Désactiver tous les boutons de réponse
+      $(".answer-button").prop("disabled", true);
+  
+      // Récupérer la réponse de l'utilisateur
+      const userAnswer = $(this).text();
+  
+      // Vérifier la réponse et ajouter des points si c'est correct
+      const correctAnswer = questions[currentQuestion].correctAnswer;
+      const points = questions[currentQuestion].points;
+      if (userAnswer === correctAnswer) {
+        userScore += points;
+      }
+  
+      // Afficher le bouton Suivant et le résultat
+      $("#next-button").show();
+      $("#result").show();
+      $("#result").text("Vous avez choisi " + userAnswer + ".");
+    });
+  }
+  // Fonction pour passer à la question suivante
+function nextQuestion() {
+    // Augmenter le compteur de question
+    currentQuestion++;
+  
+    // Vérifier si toutes les questions ont été posées
+    if (currentQuestion >= questions.length) {
+      // Afficher le résultat final
+      let resultText = "";
+      if (userScore === 15) {
+        resultText = "Woaawh! Vous avez obtenu " + userScore + " points. Bravo !";
+      } else if (userScore === 10) {
+        resultText = "Génial! Vous avez obtenu " + userScore + " points. Bravo !";
+      } else if (userScore === 5) {
+        resultText = "Bravo! Vous avez obtenu " + userScore + " points. Bravo !";
+      } else {
+        resultText = "Vous avez obtenu " + userScore + " points. Merci d'avoir joué !";
+      }
+      $("#result").text(resultText);
+      $("#next-button").hide();
+      $(".answer-button").off("click");
+    } else {
+      // Afficher la question suivante
+      displayQuestion();
+    }
+  }
--- Quizz.php
+++ Quizz.php
@@ -7,45 +7,18 @@
 	<link rel="stylesheet" href="Quizz.css">
 </head>
 <body>
-	<div class="container">
-		<h1>Mon quiz</h1>
-		<form id="quiz-form">
-			<!-- Question 1 -->
-			<div class="question">
-				<h2>Quelle est votre couleur préférée?</h2>
-				<label><input type="radio" name="q1" value="Bleu">Bleu</label>
-				<label><input type="radio" name="q1" value="Jaune">Jaune</label>
-				<label><input type="radio" name="q1" value="Rouge">Rouge</label>
-				<label><input type="radio" name="q1" value="Vert">Vert</label>
-			</div>
-			
-			<!-- Question 2 -->
-			<div class="question">
-				<h2>Quel est votre animal préféré?</h2>
-				<label><input type="radio" name="q2" value="Chien">Chien</label>
-				<label><input type="radio" name="q2" value="Chat">Chat</label>
-				<label><input type="radio" name="q2" value="Oiseau">Oiseau</label>
-				<label><input type="radio" name="q2" value="Poisson">Poisson</label>
-			</div>
-			
-			<!-- Question 3 -->
-			<div class="question">
-				<h2>Quel est votre plat préféré?</h2>
-				<label><input type="radio" name="q3" value="Sushi">Sushi</label>
-				<label><input type="radio" name="q3" value="Pizza">Pizza</label>
-				<label><input type="radio" name="q3" value="Burger">Burger</label>
-				<label><input type="radio" name="q3" value="Salade">Salade</label>
-			</div>
-			
-			<!-- Submit button -->
-			<button type="submit">Soumettre</button>
-		</form>
-		
-		<!-- Score display -->
-		<div id="score"></div>
-	</div>
-	
-	<!-- JavaScript -->
+
+<div id="quiz-container">
+  <h2 id="question"></h2>
+  <ul id="answers">
+    <li><button class="answer-button"></button></li>
+    <li><button class="answer-button"></button></li>
+    <li><button class="answer-button"></button></li>
+  </ul>
+  <button id="next-button" style="display:none">Suivant</button>
+  <p id="result" style="display:none"></p>
+</div>
+
 	<script src="Quizz.js"></script>
 </body>
 </html>
