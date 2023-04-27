d2cd8a2d465a80b54d55a5635c90f853c5d3a2c0
galathe2901 <csmaf.gouv@yahoo.fr>
2023-04-27 09:40:55 +0200

    crnrn

--- Quizz.php
+++ Quizz.php
@@ -4,52 +4,71 @@
 	<title>Mon quiz</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="Quizz.css">
+	<link rel="stylesheet" href="">
   
 </head>
 <body>
-<div class="blog-slider">
-  <div class="blog-slider__wrp swiper-wrapper">
-    <div class="blog-slider__item swiper-slide">
-      <div class="blog-slider__img">
-        
-        <img src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1535759872/kuldar-kalvik-799168-unsplash.webp" alt="">
-      </div>
-      <div class="blog-slider__content">
-        <span class="blog-slider__code">26 December 2019</span>
-        <div class="blog-slider__title">Lorem Ipsum Dolor</div>
-        <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi? </div>
-        <a href="#" class="blog-slider__button">READ MORE</a>
-      </div>
-    </div>
-    <div class="blog-slider__item swiper-slide">
-      <div class="blog-slider__img">
-        <img src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1535759871/jason-leung-798979-unsplash.webp" alt="">
-      </div>
-      <div class="blog-slider__content">
-        <span class="blog-slider__code">26 December 2019</span>
-        <div class="blog-slider__title">Lorem Ipsum Dolor2</div>
-        <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi?</div>
-        <a href="#" class="blog-slider__button">READ MORE</a>
-      </div>
-    </div>
-    
-    <div class="blog-slider__item swiper-slide">
-      <div class="blog-slider__img">
-        <img src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1535759871/alessandro-capuzzi-799180-unsplash.webp" alt="">
-      </div>
-      <div class="blog-slider__content">
-        <span class="blog-slider__code">26 December 2019</span>
-        <div class="blog-slider__title">Lorem Ipsum Dolor</div>
-        <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi?</div>
-        <a href="#" class="blog-slider__button">READ MORE</a>
-      </div>
-    </div>
-    
-  </div>
-  <div class="blog-slider__pagination"></div>
+<div id="quiz-container">
+  <h2>Question 1 : Quelle est la capitale de la France ?</h2>
+  <button id="answer-a">Londres</button>
+  <button id="answer-b">Berlin</button>
+  <button id="answer-c">Paris</button>
+  <button id="answer-d">Madrid</button>
+  <button id="next-btn" style="display: none;">Suivant</button>
 </div>
 
-<script src="Quizz.js"></script>
+<script>
+  const quizContainer = document.getElementById("quiz-container");
+  const answerButtons = quizContainer.querySelectorAll("button");
+  const nextButton = quizContainer.querySelector("#next-btn");
+
+  answerButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      // Vérifie si la réponse est correcte
+      if (button.id === "answer-c") {
+        quizContainer.innerHTML = `
+          <h2>Bonne réponse !</h2>
+          <button id="next-btn">Suivant</button>
+        `;
+      } else {
+        quizContainer.innerHTML = `
+          <h2>Mauvaise réponse...</h2>
+          <button id="next-btn">Suivant</button>
+        `;
+      }
+
+      nextButton.style.display = "block";
+    });
+  });
+
+  nextButton.addEventListener("click", () => {
+    quizContainer.innerHTML = `
+      <h2>Question 2 : Combien de planètes dans notre système solaire ?</h2>
+      <button id="answer-a">4</button>
+      <button id="answer-b">7</button>
+      <button id="answer-c">8</button>
+      <button id="answer-d">12</button>
+      <button id="next-btn" style="display: none;">Suivant</button>
+    `;
+
+    answerButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        if (button.id === "answer-c") {
+          quizContainer.innerHTML = `
+            <h2>Bonne réponse !</h2>
+            <button id="next-btn">Suivant</button>
+          `;
+        } else {
+          quizContainer.innerHTML = `
+            <h2>Mauvaise réponse...</h2>
+            <button id="next-btn">Suivant</button>
+          `;
+        }
+
+        nextButton.style.display = "block";
+      });
+    });
+  });
+</script>
 </body>
 </html>
