f3caee7cebea34cbdb381c347c0d35c6ef1bb746
galathe2901 <csmaf.gouv@yahoo.fr>
2023-04-27 08:53:54 +0200

    cnrnr

--- Quizz.php
+++ Quizz.php
@@ -4,7 +4,7 @@
 	<title>Mon quiz</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="Quizz1.scss">
+	<link rel="stylesheet" href="quizz.scss">
     <script
   src="https://code.jquery.com/jquery-3.6.4.min.js"
   integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8="
