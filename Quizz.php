a3ba4054c2ac353603b9bb6fdf85857611df5545
galathe2901 <csmaf.gouv@yahoo.fr>
2023-04-28 06:25:32 +0200

    no image

--- Quizz.php
+++ Quizz.php
@@ -1,36 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quizz app game</title>
-    <link rel="stylesheet" href="Quizz.css">
-    <script defer src="quizz.js"></script>
-</head>
-<body>
-    <header>
-        </header> 
-<div class="container">
-    <div id="container-text" class="container-text">
-    <h2>bienvenue sur le quizz </h2>
-    <p>aucune mauvaise reponse alors pas d inquettude</p>
-</div>
-    <div id="question-container" class="hide">
-    <div id="question">question</div>
-    <div id="answer-buttons" class="btn-grid">
-    <button class="btn "> ans a</button>
-    <button class="btn "> ans b</button>
-    <button class="btn "> ans c</button>
-    <button class="btn "> ans d</button>
+<div class="blog-slider">
+  <div class="blog-slider__wrp swiper-wrapper">
+    <div class="blog-slider__item swiper-slide">
+      <div class="blog-slider__img">
+        
+        <img src="" alt="">
+      </div>
+      <div class="blog-slider__content">
+        <span class="blog-slider__code">26 December 2019</span>
+        <div class="blog-slider__title">Lorem Ipsum Dolor</div>
+        <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi? </div>
+        <a href="#" class="blog-slider__button">READ MORE</a>
+      </div>
     </div>
+    <div class="blog-slider__item swiper-slide">
+      <div class="blog-slider__img">
+        <img src="" alt="">
+      </div>
+      <div class="blog-slider__content">
+        <span class="blog-slider__code">26 December 2019</span>
+        <div class="blog-slider__title">Lorem Ipsum Dolor2</div>
+        <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi?</div>
+        <a href="#" class="blog-slider__button">READ MORE</a>
+      </div>
+    </div>
+    
+    <div class="blog-slider__item swiper-slide">
+      <div class="blog-slider__img">
+        <img src="" alt="">
+      </div>
+      <div class="blog-slider__content">
+        <span class="blog-slider__code">26 December 2019</span>
+        <div class="blog-slider__title">Lorem Ipsum Dolor</div>
+        <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi?</div>
+        <a href="#" class="blog-slider__button">READ MORE</a>
+      </div>
+    </div>
+    
+  </div>
+  <div class="blog-slider__pagination"></div>
 </div>
-    <div id="score" class= "grade hide"> SCORE: 20%</div> 
-
-    <div class="controls">
-        <button id="start-btn" class="start-btn btn">Start</button>
-        <button id="next-btn" class="next-btn btn hide" >Next</button>
-        <button id="gameover-btn" class="gameover-btn btn hide">Game-over</button>
-    </div> 
-</div>
-</body>
-</html>
