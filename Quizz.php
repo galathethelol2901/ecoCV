e7eea6a8cc00e9bcc87aea86bec6cbbf406a8741
galathe2901 <csmaf.gouv@yahoo.fr>
2023-04-28 05:29:43 +0200

    cnrnr

--- Quizz.php
+++ Quizz.php
@@ -16,16 +16,22 @@
     <input type="radio" name="q1" value="r2">Une entreprise en open source, où la collaboration et le partage sont valorisés.<br>
     <input type="radio" name="q1" value="r3">Une entreprise axée sur le bien-être des employés, offrant des avantages tels que des horaires flexibles, des espaces de détente, etc.<br>
     <input type="radio" name="q1" value="r4">Je suis visiteur.<br>
+    <button onclick="submitAnswer('q1')">Submit</button>
+    <p class="result1"></p>
     <h3>Question 2 : Quel niveau d'études attendez-vous d'un développeur ?</h3>
     <input type="radio" name="q2" value="r1">Bac+5 minimum.<br>
     <input type="radio" name="q2" value="r2">Autodidacte et passionné.<br>
     <input type="radio" name="q2" value="r3">Niveau d'études indifférent, l'expérience professionnelle prime.<br>
     <input type="radio" name="q2" value="r4">Je suis visiteur.<br>
+    <button onclick="submitAnswer('q2')">Submit</button>
+    <p class="result2"></p>
     <h3>Question 3 : Quel salaire proposeriez-vous à un développeur ?</h3>
     <input type="radio" name="q3" value="r1">Selon son niveau d'expérience et ses compétences.<br>
     <input type="radio" name="q3" value="r2">100 bitcoins.<br>
     <input type="radio" name="q3" value="r3">Un salaire minimum ou juste au-dessus.<br>
     <input type="radio" name="q3" value="r4">Je suis visiteur.<br>
+    <button onclick="submitAnswer('q3')">Submit</button>
+    <p class="result3"></p>
   </form>
   
   <script src="quizz.js"></script>
