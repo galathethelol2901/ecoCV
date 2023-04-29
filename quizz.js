1c02568211b1504aea04553452eab37efaebb0f0
galathe2901 <csmaf.gouv@yahoo.fr>
2023-04-29 02:09:29 +0200

    score

--- quizz.js
+++ quizz.js
@@ -1,74 +1,95 @@
+const startButton = document.getElementById("start");
+const restartButton = document.getElementById("restart");
+const questionContainer = document.getElementById("question-container");
+const scoreContainer = document.getElementById("score-container");
+const questionElement = document.getElementById("question");
+const choiceButtons = document.querySelectorAll(".btn");
+const scoreElement = document.getElementById("score");
+const totalElement = document.getElementById("total");
+
+let shuffledQuestions, currentQuestionIndex, score;
+
 const questions = [
   {
-    question: "Question 1",
-    options: [
-      { text: "Réponse A", points: 1 },
-      { text: "Réponse B", points: 2 },
-      { text: "Réponse C", points: 3 },
-      { text: "Réponse D", points: 4 },
+    question: "Dans quelle ville se trouve la tour Eiffel ?",
+    answers: [
+      { text: "Londres", points: 0 },
+      { text: "Berlin", points: 0 },
+      { text: "Paris", points: 10 },
+      { text: "Madrid", points: 0 },
     ],
   },
   {
-    question: "Question 2",
-    options: [
-      { text: "Réponse A", points: 1 },
-      { text: "Réponse B", points: 2 },
-      { text: "Réponse C", points: 3 },
-      { text: "Réponse D", points: 4 },
+    question: "Quelle est la capitale de l'Espagne ?",
+    answers: [
+      { text: "Londres", points: 0 },
+      { text: "Berlin", points: 0 },
+      { text: "Paris", points: 0 },
+      { text: "Madrid", points: 10 },
     ],
   },
 ];
-
-const startButton = document.getElementById("start-btn");
-const questionContainer = document.getElementById("question-container");
-const questionElement = document.getElementById("question");
-const optionsContainer = document.getElementById("options-container");
-const scoreContainer = document.getElementById("score-container");
-const scoreElement = document.getElementById("score");
-const restartButton = document.getElementById("restart-btn");
-
-let currentQuestionIndex = 0;
-let score = 0;
 
 startButton.addEventListener("click", startQuiz);
 restartButton.addEventListener("click", restartQuiz);
 
 function startQuiz() {
   startButton.style.display = "none";
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  score = 0;
+  scoreElement.innerText = score;
+  totalElement.innerText = shuffledQuestions.length * 10;
   questionContainer.style.display = "block";
-  showQuestion();
+  setNextQuestion();
 }
 
-function showQuestion() {
-  const currentQuestion = questions[currentQuestionIndex];
-  questionElement.innerText = currentQuestion.question;
-  optionsContainer.innerHTML = "";
-  currentQuestion.options.forEach((option) => {
-    const button = document.createElement("button");
-    button.innerText = option.text;
-    button.classList.add("btn");
-    button.addEventListener("click", () => {
-      score += option.points;
-      currentQuestionIndex++;
-      if (currentQuestionIndex < questions.length) {
-        showQuestion();
-      } else {
-        showScore();
-      }
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer, index) => {
+    choiceButtons[index].innerText = answer.text;
+    choiceButtons[index].addEventListener("click", () => {
+      selectAnswer(answer.points);
     });
-    optionsContainer.appendChild(button);
   });
+}
+
+function resetState() {
+  choiceButtons.forEach((button) => {
+    button.classList.remove("correct");
+    button.classList.remove("incorrect");
+    button.removeEventListener("click", () => {});
+  });
+}
+
+function selectAnswer(points) {
+  score += points;
+  scoreElement.innerText = score;
+  Array.from(choiceButtons).forEach((button) => {
+    button.classList.add(button.dataset.correct ? "correct" : "incorrect");
+  });
+  if (currentQuestionIndex === shuffledQuestions.length - 1) {
+    showScore();
+  } else {
+    currentQuestionIndex++;
+    setNextQuestion();
+  }
 }
 
 function showScore() {
   questionContainer.style.display = "none";
   scoreContainer.style.display = "block";
-  scoreElement.innerText = score;
 }
 
 function restartQuiz() {
-currentQuestionIndex = 0;
-score = 0;
-scoreContainer.style.display = "none";
-startButton.style.display = "block";
+  currentQuestionIndex = 0;
+  score = 0;
+  scoreElement.innerText = score;
+  scoreContainer.style.display = "none";
+  startButton.style.display = "block";
 }
