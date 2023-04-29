666f308f1da21eaedd45d988b459b485ce5c960f
galathe2901 <csmaf.gouv@yahoo.fr>
2023-04-29 02:36:07 +0200

    cnnr

--- quizz.js
+++ quizz.js
@@ -7,15 +7,9 @@ const choiceButtons = document.querySelectorAll(".btn");
 const scoreElement = document.getElementById("score");
 const totalElement = document.getElementById("total");
 
-let questions = [
-  {
-    question: "Dans quelle ville se trouve la tour Eiffel ?",
-    answers: [
-      { text: "Londres", points: 0 },
-      { text: "Berlin", points: 0 },
-      { text: "Paris", points: 10 },
-      { text: "Madrid", points: 0 },
-    ],
+let shuffledQuestions, currentQuestionIndex, score;
+
+const questions = [  {    question: "Dans quelle ville se trouve la tour Eiffel ?",    answers: [      { text: "Londres", points: 0 },      { text: "Berlin", points: 0 },      { text: "Paris", points: 10 },      { text: "Madrid", points: 0 },    ],
   },
   {
     question: "Quelle est la capitale de l'Espagne ?",
@@ -27,8 +21,6 @@ let questions = [
     ],
   },
 ];
-
-let shuffledQuestions, currentQuestionIndex, score;
 
 startButton.addEventListener("click", startQuiz);
 restartButton.addEventListener("click", restartQuiz);
@@ -51,50 +43,31 @@ function setNextQuestion() {
 
 function showQuestion(question) {
   questionElement.innerText = question.question;
-  for (let i = 0; i < choiceButtons.length; i++) {
-    const button = choiceButtons[i];
-    button.innerText = question.answers[i].text;
-    button.removeEventListener('click', handleClick);
-    button.addEventListener('click', handleClick);
-  }
-}
-
-function handleClick(event) {
-  const selectedAnswer = event.target;
-  const points = parseInt(selectedAnswer.dataset.points);
-  score += points;
-  scoreElement.innerText = score;
-  Array.from(choiceButtons).forEach(button => {
-    if (button.dataset.points == "10") {
-      button.classList.add('correct');
-    } else {
-      button.classList.add('incorrect');
-    }
+  question.answers.forEach((answer, index) => {
+    choiceButtons[index].innerText = answer.text;
+    choiceButtons[index].dataset.points = answer.points;
+    choiceButtons[index].addEventListener("click", selectAnswer);
   });
-  choiceButtons.forEach(button => {
-    button.removeEventListener('click', handleClick);
-  });
-  if (currentQuestionIndex === shuffledQuestions.length - 1) {
-    showScore();
-  } else {
-    currentQuestionIndex++;
-    setNextQuestion();
-  }
 }
 
 function resetState() {
   choiceButtons.forEach((button) => {
     button.classList.remove("correct");
     button.classList.remove("incorrect");
-    button.removeEventListener("click", () => {});
+    button.removeEventListener("click", selectAnswer);
   });
 }
 
-function selectAnswer(points) {
+function selectAnswer(event) {
+  const selectedButton = event.target;
+  const points = Number(selectedButton.dataset.points);
   score += points;
   scoreElement.innerText = score;
   Array.from(choiceButtons).forEach((button) => {
-    button.classList.add(button.dataset.correct ? "correct" : "incorrect");
+    button.classList.add(button.dataset.points > 0 ? "correct" : "incorrect");
+  });
+  choiceButtons.forEach((button) => {
+    button.removeEventListener("click", selectAnswer);
   });
   if (currentQuestionIndex === shuffledQuestions.length - 1) {
     showScore();
@@ -107,15 +80,18 @@ function selectAnswer(points) {
 function showScore() {
   questionContainer.style.display = "none";
   scoreContainer.style.display = "block";
+  const totalPoints = shuffledQuestions.length * 10;
+  totalElement.innerText = totalPoints;
+  scoreElement.innerText = score;
+  const percentage = ((score / totalPoints) * 100).toFixed(2);
+  questionElement.innerText = `Vous avez obtenu ${percentage}% des points !`;
 }
 
 function restartQuiz() {
   currentQuestionIndex = 0;
   score = 0;
   scoreElement.innerText = score;
-  totalElement.innerText = shuffledQuestions.length * 10;
   scoreContainer.style.display = "none";
   questionContainer.style.display = "block";
-  shuffledQuestions = questions.slice().sort(() => Math.random() - 0.5);
   setNextQuestion();
 }
