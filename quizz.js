b2d558e954a0ad937a5f27634086efbd9dea9f2c
galathe2901 <csmaf.gouv@yahoo.fr>
2023-04-30 16:38:09 +0200

    cnrn

--- quizz.js
+++ quizz.js
@@ -125,13 +125,17 @@ function showScore() {
   const totalPoints = shuffledQuestions.length * 10;
   totalElement.innerText = " sur " + totalPoints;
   const percentage = ((score / totalPoints) * 100).toFixed(2);
-  if (score >= 70) {
-    questionElement.innerText = `GREAT! You scored ${percentage}%!`;
-  } else if (score <= 50) {
-    questionElement.innerText = `WOAW gg! You scored ${percentage}%!`;
+  let message = "";
+  if (score >= 90) {
+    message = "Wonderful! ";
+  } else if (score >= 70) {
+    message = "Very good! ";
+  } else if (score >= 50) {
+    message = "Great job! ";
   } else {
-    questionElement.innerText = `Not bad! You scored ${percentage}%!`;
+    message = "Oops! ";
   }
+  questionElement.innerText = message + `You scored ${percentage}%!`;
   scoreElement.innerText = score;
 }
 
@@ -142,23 +146,4 @@ function restartQuiz() {
   scoreContainer.style.display = "none";
   questionContainer.style.display = "block";
   setNextQuestion();
-}
-
-// récupérer les éléments score et total à partir du DOM
-const scoreElem = document.getElementById('score');
-const totalElem = document.getElementById('total');
-
-// récupérer les scores de l'utilisateur et du total depuis les éléments du DOM
-const scoreValue = parseInt(scoreElem.innerText);
-const totalValue = parseInt(totalElem.innerText);
-
-// afficher un message personnalisé en fonction du score de l'utilisateur
-if (scoreValue / totalValue >= 0.9) {
-  alert("Félicitations, vous avez obtenu un score excellent !");
-} else if (scoreValue / totalValue >= 0.7) {
-  alert("Bravo, vous avez obtenu un bon score !");
-} else if (scoreValue / totalValue >= 0.5) {
-  alert("Pas mal, vous avez obtenu un score moyen.");
-} else {
-  alert("Dommage, vous n'avez pas obtenu un bon score.");
 }
