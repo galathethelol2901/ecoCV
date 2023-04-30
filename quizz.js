6c093fb9b1d905894c8ff4bbc049b0f5e2ab6711
galathe2901 <csmaf.gouv@yahoo.fr>
2023-04-30 17:16:55 +0200

    ncnrnr

--- quizz.js
+++ quizz.js
@@ -7,6 +7,8 @@ const choiceButtons = document.querySelectorAll(".btn");
 const scoreElement = document.getElementById("score");
 const totalElement = document.getElementById("total");
 const nextButton = document.getElementById("next-btn");
+const resultMessage = document.getElementById("result-message");
+
 
 let shuffledQuestions, currentQuestionIndex, score;
 
