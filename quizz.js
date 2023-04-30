7efc878059241a67e518c3ed5ec12b7950ab0b5b
galathe2901 <csmaf.gouv@yahoo.fr>
2023-04-30 04:57:14 +0200

    5ue

--- quizz.js
+++ quizz.js
@@ -12,36 +12,45 @@ let shuffledQuestions, currentQuestionIndex, score;
 
 const questions = [  
   {    
-  question: "Quelle est votre vision de l'entreprise idéale ?",    
+  question: "PARMIS CEST CHOIX, Dans quelle circomstance etes vous arrivez sur ce quizz ?",    
   answers: [      
-    { text: "visiteur", points: 0 },      
-    { text: "Une entreprise qui accorde la priorité aux profits avant tout", points: 5 },      
-    { text: "Une entreprise qui valorise la liberté et l'autonomie de ses employés", points: 10 },      
-    { text: "Une entreprise qui vise un juste milieu entre profits et bien-être des employés", points: 20 },    
+    { text: "Je me suis perdu dans les méandres de l'Internet, comme d'habitude alors VISITEUR", points: 0 },      
+    { text: "je viens voir ce que propose les devs concurrents", points: 5 },      
+    { text: "dans l'optique de demarchez un potentielle candidat", points: 10 },      
+    { text: "JE cherche un DEV d'URGENCE !!! ", points: 20 },    
      ],
   },
-{
-    question: " Quel niveau d'études recherchez-vous chez vos employés ?",
+  {
+    question: "PARMIS CEST CHOIX, Quelle est votre vision de l'entreprise idéale ?",
     answers: [
-      { text: "visiteur ", points: 0 },
+      { text: "Une entreprise où tout le monde est payé en tacos ,donc ont dit VISITEUR hein ? ", points: 0 },
+      { text: "Une entreprise qui accorde la priorité aux profits avant tout", points: 5 },
+      { text: "Une entreprise qui valorise la liberté et l'autonomie de ses employés", points: 10 },
+      { text: "Une entreprise qui vise un juste milieu entre profits et bien-être des employés", points: 20 },
+    ],
+  },
+{
+    question: "PARMIS CEST CHOIX, Quel niveau d'études recherchez-vous chez vos employés ?",
+    answers: [
+      { text: "si il a youtube je prend ! donc comme toujours VISITEUR ! ", points: 0 },
       { text: "Master +5 : j'exige un niveau élevé d'études supérieures", points: 5 },
       { text: "Je valorise plus l'expérience professionnelle que le niveau d'études ", points: 10 },
       { text: "je suis ouvert à tous les profils, y compris les autodidactes ", points: 20 },
     ],
   },
   {    
-    question: "salaire?",    
+    question: "PARMIS CEST CHOIX, Quel type de salaire proposez-vous ?",    
     answers: [      
-      { text: "visiteur", points: 0 },      
-      { text: "10 bitcoin : une rémunération basée sur une crypto-monnaie pourrait être intéressante ", points: 5 },      
-      { text: "une rémunération qui tient compte des compétences, de l'expérience et des réalisations de l'employé", points: 10 },      
-      { text: "une rémunération compétitive par rapport aux normes de l'industrie ", points: 20 },    
+      { text: "100 bitcoin soit 262001.6e par mois , ok ok VISITEUR ! ", points: 0 },      
+      { text: "Un salaire fixe mensuel, peu importe le travail accompli", points: 5 },      
+      { text: "Une rémunération variable basée sur les résultats de l'entreprise", points: 10 },      
+      { text: "Une combinaison de salaire fixe et variable en fonction de la performance individuelle et de l'entreprise ", points: 20 },    
        ],
     },
     {    
-      question: "jeux favorit  ?",    
+      question: "PARMIS CEST CHOIX, Quel est votre jeu favori ?",    
       answers: [      
-        { text: "visiteur", points: 0 },      
+        { text: "le jeu de la vie ! haha comique ou VISITEUR ?", points: 0 },      
         { text: "J'aime jouer à des jeux pour me détendre", points: 5 },      
         { text: "J'aime les jeux de réflexion pour leur complexité", points: 10 },      
         { text: "J'aime les jeux open-world pour leur liberté de création", points: 20 },    
