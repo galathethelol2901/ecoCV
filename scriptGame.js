132f545e778ef43162d5335ca96d2cf67c8b6e74
galathe2901 <csmaf.gouv@yahoo.fr>
2023-05-02 14:36:48 +0200

    nrrn

--- scriptGame.js
+++ scriptGame.js
@@ -193,7 +193,7 @@ let adjustX = -3;
 let adjustY = -3;
 ctx.fillStyle = 'white';
 ctx.font = '17px Verdana';
-ctx.fillText('LIQUID', 20, 42);
+ctx.fillText('ECOPROG6', 18, 40);
 //ctx.font = '19px Verdana';
 //ctx.fillText('TEXT', 36, 49);
 const textCoordinates = ctx.getImageData(0, 0, 100, 100);
