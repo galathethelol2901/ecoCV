2607f2ea9d2869ab9c47e413ca06afe401d2db1c
galathe2901 <csmaf.gouv@yahoo.fr>
2023-05-03 17:22:41 +0200

    dndndn

--- Quizz.php
+++ Quizz.php
@@ -11,8 +11,6 @@
 </head>
 <body>
   <header>
-    <h1>bienvenue sur le ULTRA fast quizz ! </h1>
-    <h4>pour ce site , pas de pression , pas de mauvaise reponse et pas de temps limiter, la seule limite est la curiositer<h4>
   </header>
 
   <div id="quiz-container">
