c32105dac698c49da0db0452b2bdd5fd6302c313
galathe2901 <csmaf.gouv@yahoo.fr>
2023-05-03 14:33:34 +0200

    ndnd

--- Quizz.php
+++ Quizz.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quizz app game</title>
     <link rel="stylesheet" href="Quizz.css">
-    <script src="https://kit.fontawesome.com/3e7bbf4130.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" 
+    integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" 
+    crossorigin="anonymous"/>
     <script defer src="quizz.js"></script>
 </head>
 <body>
