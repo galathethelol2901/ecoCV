e75b42c0289b973a86c6cc0744d8e643029f123c
galathe2901 <csmaf.gouv@yahoo.fr>
2023-05-06 21:26:01 +0200

    cv

---
+++ indexENG.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>WELCOME ON ECOPROG6 CV</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="indexColor.css">
+  </head>
+  <body>
+    <div class="mouse cursor"></div>
+    <div class="mouse follow-cursor first"></div>
+    <div class="mouse follow-cursor second"></div>
+    <div class="mouse follow-cursor third"></div>
+    <div class="mouse follow-cursor fourth"></div>
+    <div class="mouse follow-cursor fifth"></div>
+    <div class="header">
+      <div class="inner-header flex">
+        <h1> WELCOME ON ECOPROG6 <br>
+          <small>for now it's a CV <small>
+        </h1>
+        <div class="container">
+          <div class="btn">
+            <a href="main60sENG.php">ENTER for 45s</a>
+          </div>
+        </div>
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false"> select lang </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <a class="dropdown-item" href="indexCV.php">FR</a>
+          </div>
+        </div>
+      </div>
+      <div>
+        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+          <defs>
+            <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+          </defs>
+          <g class="parallax">
+            <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+            <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+            <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+            <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+          </g>
+        </svg>
+      </div>
+    </div>
+    <div class="content flex">
+      <p> Read between the waves before diving. cause you can visit only ONE time !</p>
+      <button type="button" class="btn btn-link bg-transparent" data-bs-toggle="modal" data-bs-target="#modal1">
+        <span class="bi bi-info-circle" title="RULES SITE" id="icon1"></span>
+      </button>
+      <div class="modal fade" id="modal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">how it works !?</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">  How the site works: <br>
+                        The time is limited to 45s.<br>
+                        You can visit it only once !!!<br>
+                        If you liked it, don’t hesitate to share it.<br>
+                        <br>
+                        Have a nice visit !! <br>
+                        <br>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button type="button" class="btn btn-link bg-transparent" data-bs-toggle="modal" data-bs-target="#modal2">
+        <span class="bi bi-diagram-3-fill" title="CALL ME " id="icon2"></span>
+      </button>
+      <div class="modal fade" id="modal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">contact</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">Yoan Mettoudi <br>
+                    30 year<br>
+                    06 36 98 50 85 <br>
+                    ymettoudi@yahoo.fr<br>
+
+                    Skype(c), Zoom(c), and my phone are available 24/7.<br>
+                    <br>
+                    <br>
+                    My tools are an Intel Core i7 laptop with 8GB of RAM,<br>
+                     and a graphics card that is not up to par. However, <br>
+                     this is not a real problem as everything is modifiable on my PC.<br>
+
+                    I use PHPStorm(c) and VScode(c) to write my code.<br>
+                     Github would allow me to send it to you, <br>
+                     unless you have a corporate network, in this case an SSH key would be necessary."
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button type="button" class="btn btn-link bg-transparent" data-bs-toggle="modal" data-bs-target="#modal3">
+        <span class="bi bi-clipboard-plus" title="INFO SITE" id="icon3"></span>
+      </button>
+      <div class="modal fade" id="modal3" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">the little extras </h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body"> This site was created by me alone.<br>
+                        No cms, no wordpress. <br>
+                        Inspared by many creators, <br>
+                        i’m trying to re-code myself <br>
+                        and according to my choices any idea that<br>
+                        sounds cool to me.<br>
+                        168h -or 7 days- was necessary before the upload.  <br>
+                        <br>
+                        <br>
+                        In my opinion, there are 3 ways to visit without limit ! <br>
+                        <br>
+                        - ctrl + impr Ecr <br>
+                        - take a picture of it<br>
+                        - modify the script in less than 45s. <br>
+                        <br>
+                        <br>
+                        If you find another way, text me ! <br>
+                        This list will be renewed every time !
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
+      <script type="text/javascript">
+        $("body").mousemove(function(e) {
+          let mouse = $(".mouse");
+          mouse.css({
+            left: e.pageX,
+            top: e.pageY,
+            opacity: 1,
+          });
+        });
+        $("a").hover(function() {
+          $(".mouse").toggleClass("hover");
+        });
+      </script>
+  </body>
+</html>
---
+++ main60sEng.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="fr">
+	<![endif]-->
+<!--[if IE 7]>
+	<html class="no-js lt-ie9 lt-ie8" lang="fr">
+		<![endif]-->
+<!--[if IE 8]>
+		<html class="no-js lt-ie9" lang="fr">
+			<![endif]-->
+<!--[if gt IE 8]>
+			<html class="no-js" lang="fr">
+				<!-
+				<![endif]-->
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>ECOPROG6 THE CV</title>
+    <meta name="description" content="Mon super CV">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="stylemain60.css">
+    <script>
+      if (sessionStorage.getItem('main60sEng') === '1') {
+        window.location = "indexCV.php";
+      }
+      sessionStorage.setItem('main60sEng', '1');
+    </script>
+  </head>
+  <section class="sticky">
+    <div class="bubbles">
+      <div class="bubble"></div>
+      <div class="bubble"></div>
+      <div class="bubble"></div>
+      <div class="bubble"></div>
+      <div class="bubble"></div>
+      <div class="bubble"></div>
+      <div class="bubble"></div>
+      <div class="bubble"></div>
+      <div class="bubble"></div>
+      <div class="bubble"></div>
+    </div>
+  </section>
+  <body>
+    <section class="profile section-padding">
+      <div class="resume-wrapper">
+        <div class="picture-resume-wrapper">
+          <div class="picture-resume">
+            <span>
+              <img src="photodemoi.jpg" alt="photo de moi">
+            </span>
+          </div>
+          <div class="clearfix"></div>
+        </div>
+        <div class="name-wrapper">
+          <h1> Mettoudi <br /> Yoan </h1>
+        </div>
+        <div class="clearfix"></div>
+        <div class="contact-info clearfix">
+          <ul class="list-titles">
+            <li>Call</li>
+            <li>Mail</li>
+            <li>Web</li>
+          </ul>
+          <ul class="list-content ">
+            <li>06 36 98 50 85 </li>
+            <li>ymettoudi@yahoo.fr</li>
+            <li>Paris, France</li>
+          </ul>
+        </div>
+        <div class="contact-presentation">
+          <p> HEY ! I am looking for a Junior / Full Stack Dev job. <br> Preferably in telecommuting. Creative and ingenious in <br> front-end, serious in back-end, and used to teamwork. <br> I finish my training web & data. Passionate <br> about cryptology. Self-taught and diligent, <br> I work as a blockchain in a secure and reliable way. <br>
+            <br> Learning new programming languages and discovering tomorrow's IT innovations <br> allows me to be versatile and always on the lookout. <br> I would have no problem adapting to your usual software <br> and would not hesitate to share my discoveries with you if it is useful. <br>
+          </p>
+        </div>
+        <div class="contact-social clearfix">
+          <ul class="list-titles">
+            <li>LinkedIn</li>
+            <li>GitHub</li>
+            <li>Codepen</li>
+          </ul>
+          <ul class="list-content">
+            <li>
+              <a href="https://www.linkedin.com/in/yoan-mettoudi-ab519b119/" target="_blank">Account LinkedIn</a>
+            </li>
+            <li>
+              <a href="https://github.com/galathe2901" target="_blank">Account GitHub</a>
+            </li>
+            <li>
+              <a href="https://codepen.io/galathe2901" target="_blank">Account Codepen</a>
+            </li>
+          </ul>
+        </div>
+        <br />
+        <div class="contact-social clearfix">
+          <h2>Formation </h2>
+        </div>
+        <div class="contact-social clearfix">
+          <ul class="list-titles">
+            <li>STUDI (formation dev web fullstack)</li>
+            <li>EPMTTH (hotel school)</li>
+          </ul>
+          <ul class="list-content">
+            <li>
+              <a href="https://www.studi.com/fr" target="_blank"> STUDI </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section class="experience section-padding">
+      <div class="container">
+        <div class="section-wrapper clearfix">
+          <h3 class="section-title">Skills</h3>
+          <ul>
+            <li class="skill-percentage">HTML / HTML5</li>
+            <li class="skill-percentage">CSS / CSS3 / BOOTSTRAP</li>
+            <li class="skill-percentage">AngularJS</li>
+            <li class="skill-percentage">Jquery</li>
+            <li class="skill-percentage">Javascript</li>
+            <li class="skill-percentage">BASIC PHP</li>
+            <li class="skill-percentage">photoshop</li>
+            <li class="skill-percentage">BASIC MYSQL</li>
+            <li class="skill-percentage">BASIC LINUX</li>
+            <li class="skill-percentage">GIT/GITHUB</li>
+            <li class="skill-percentage">GULP/GRUNT</li>
+          </ul>
+        </div>
+        <div class="section-wrapper clearfix">
+          <h3 class="section-title">&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;Hobbies</h3>
+          <p> &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;E-sport / Films / Football </p>
+        </div>
+        <div class="section-wrapper clearfix">
+          <h3 class="section-title">&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;Extra</h3>
+          <p> &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; This site was created <br> &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; using a framework,which <br> &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; allows me not to have to <br> &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; pay for a DataNameServer <br> &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; that was in my capacity <br> &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; but not in my budget.It <br> &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; does not contains cookies <br> &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; or database only for <br> &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; security reasons and <br> &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; security is my PRIORITY . </p>
+        </div>
+        <div class="clearfix"></div>
+        <h3 class="experience-title"> Expériences </h3>
+        <section class="experience section-padding">
+          <div class="container">
+            <div class="experience-wrapper">
+              <div class="company-wrapper clearfix">
+                <div class="experience-title"> Logilog </div>
+                <div class="time"> 03/2022 <br> CDD </div>
+                <div class="company-description"> SOFTWARE <br>
+                  <br> Setting up a database <br> in the objective to order <br> and retrieve data. <br>
+                </div>
+              </div>
+            </div>
+            <div class="experience-wrapper">
+              <div class="company-wrapper clearfix">
+                <div class="experience-title"> EVERSOFT</div>
+                <div class="time">01/2022 <br> STAGE </div>
+                <div class="company-description">FRONTEND <br>
+                  <br> Collaboratively creating with <br> UI and UX designers <br> for a foreign company <br> (eversoft.sg). <br>
+                </div>
+              </div>
+            </div>
+            <div class="experience-wrapper">
+              <div class="company-wrapper clearfix">
+                <div class="experience-title"> STUDI</div>
+                <div class="time">01/2021 FORMATION</div>
+                <div class="company-description">FULLSTACK <br>
+                  <br> learning <br> css html <br> database basics <br> frontend backend
+                </div>
+              </div>
+            </div>
+            <div class="clearfix"></div>
+          </div>
+        </section>
+        <div class="textEND">
+          <img src="Untitled.png" alt="" width="200px"> <?php
+            $handle = fopen("counter.txt", "r");
+            $handle = fopen("main60sEng.php", "r");
+            if(!$handle){
+                echo "Could not open the file" ;
+            }
+            else {
+                $counter = ( int ) fread ($handle,20) ;
+                fclose ($handle) ;
+                $counter++ ;
+                echo" 
+																																																						<p>
+                   since 59min this page has been visited ". $counter . " time 
+																																																							<br>
+																																																								<br>
+																																																								</p> " ;
+                $handle = fopen("counter.txt", "w" ) ;
+                $handle = fopen("main60sEng.php", "w" ) ;
+                fwrite($handle,$counter) ;
+                fclose ($handle) ;
+            }
+            ?> <div5>&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; YOU HAVE : <span id="timer"></span> S </div5>
+          <div class="bubbles">
+            <div class="bubble"></div>
+            <div class="bubble"></div>
+            <div class="bubble"></div>
+            <div class="bubble"></div>
+            <div class="bubble"></div>
+            <div class="bubble"></div>
+            <div class="bubble"></div>
+            <div class="bubble"></div>
+            <div class="bubble"></div>
+            <div class="bubble"></div>
+          </div>
+          <script type="text/javascript">
+            setTimeout(function() {
+              window.top.location = "pluis60s.html"
+            }, 45000);
+          </script>
+          <script src="javatimer60.js"></script>
+        </div>
+    </section>
+  </body>
+</html>
